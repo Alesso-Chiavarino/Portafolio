73993abd4e10774147c89ed7b3987991e9a87176
Alesso-Chiavarino <alessochiavarino@gmail.com>
2023-02-08 16:22:51 -0300

    linkedin

--- src/components/FooterIcons/FooterIcons.jsx
+++ src/components/FooterIcons/FooterIcons.jsx
@@ -8,6 +8,9 @@ const FooterIcons = () => {
             <a href='https://github.com/Alesso-Chiavarino' target="_blank">
                 <i className='github fa fa-github'></i>
             </a>
+            <a href='https://linkedin.com/in/alessandro-chiavarino-a93a37235/' target="_blank">
+                <i className='twitter fa fa-linkedin'></i>
+            </a>
             <a href='https://twitter.com/alesso_1912' target="_blank">
                 <i className='twitter fa fa-twitter'></i>
             </a>
