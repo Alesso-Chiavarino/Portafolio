35fc650fa554034ff815451faa1ce3749da912e3
alesso-chiavarino <alessochiavarino@gmail.com>
2023-05-24 11:37:31 -0300

    deploy error

--- src/api/mail.api.js
+++ src/api/mail.api.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export const sendMailsRequest = async (data) => {
     console.log(data)
-    return await axios.post('https://portafolio-server-ruby.vercel.app/api/mails', data)
+    // return await axios.post('https://portafolio-server-ruby.vercel.app/api/mails', data)
 }
