2a9dd51c8741f51e88072c857dc3820c4c1683bb
Alesso-Chiavarino <alessochiavarino@gmail.com>
2023-08-14 12:02:12 -0300

    fixing cors policy

--- server/index.js
+++ server/index.js
@@ -10,9 +10,7 @@ const serverUri = `http://localhost:${PORT}`
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-    origin: ['http://localhost:5173', 'https://alessochiavarino.vercel.app/']
-}));
+app.use(cors());
 app.use('/api', appRoutes);
 
 app.listen(PORT, () => LogColors.logBlue(`server is running on ${serverUri}`))
