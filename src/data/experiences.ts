bf7feb69574fc2c14fa67782ca0cfe4398247df5
alesso-chiavarino <alessochiavarino@gmail.com>
2024-03-05 16:09:39 -0300

    Add experiences section

---
+++ src/data/experiences.ts
@@ -0,0 +1,25 @@
+import { ExperienceType } from '../types/Experiences'
+
+export const experiences = [
+    {
+        date: 'APR 2022 - PRE',
+        title: 'Informatics Engineering Student',
+        subtitle: 'in Blas Pascal',
+        description: 'Transformation and administration of information through the design, development and application of technology.',
+        type: ExperienceType.education
+    },
+    {
+        date: 'APR 2022 - JUN 2023',
+        title: 'Full Stack Developer Course',
+        subtitle: 'in Coderhouse',
+        description: 'Developing software with React, Typescript, Node.js, Express, MongoDB.',
+        type: ExperienceType.education
+    },
+    {
+        date: 'SEP 2023 - PRE',
+        title: 'Front End Developer',
+        subtitle: 'in Raona',
+        description: 'Developing software with React with Typescript, .Net, Azure, DevOps, Sharepoint Framework.',
+        type: ExperienceType.work
+    }
+]
